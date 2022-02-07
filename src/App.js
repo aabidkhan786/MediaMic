075a812e14a057ce79bbd306f7f60cd1bf25bd3f
Aabid <abidkhan786@outlook.com>
2022-02-07 22:33:57 +0530

    Finally News API get Accessed V1.2

--- src/App.js
+++ src/App.js
@@ -5,43 +5,47 @@ import Navbar from './Components/Navbar';
 function App() {
 
   const [headline, setHeadline] = useState([]);
-
   //This piece of code doesn't workout but when i changed it's function name it get triggerd. 
   // const getNews = async () => {
   //   const response = await fetch("https://api.github.com/user");
   //   setNewz(await response.json());
   // }
 
-  const getUsers = async () => {
-    const response = await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=8fc5d29e9f8147968ccc87304f7669a0");
-    setHeadline(await response.json());
+  const getUsers = () => {
+    fetch("https://newsapi.org/v2/everything?q=india&apiKey=8fc5d29e9f8147968ccc87304f7669a0")
+      .then(response => response.json())
+      .then(data => setHeadline(data.articles))
   }
+
+
+  // const getUsers = async () => {
+  //   let response = await fetch('https://newsapi.org/v2/everything?q=india&apiKey=8fc5d29e9f8147968ccc87304f7669a0')
+  //   let data = await (response.json())
+  //   setHeadline(data.headline);
+  // }
 
   useEffect(() => {
     getUsers();
-  }, [])
+  },[])
   return (
     <>
       <Navbar title="Media Mic - A News App" />
       <div className="container my-3">
-        {
-          headline.map((newget) => {
-            return (
-              <div className="row my-3 mx-3 d-inline-flex justify-content-center">
-                <div className="col-md-3 my-2" key={newget.id}>
-                  <div className="card" style={{ width: "18rem" }}>
-                    <img src={newget.urlToImage} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                      <h5 className="card-title">{newget.title}</h5>
-                      <p className="card-text">Description: {newget.description}</p>
-                      {/* <a href="/" className="btn btn-sm btn-primary">Go somewhere</a> */}
-                    </div>
+        {headline.map((newget) => {
+          return (
+            <div className="row my-3 mx-3 d-inline-flex justify-content-center" key={newget.url}>
+              <div className="col-md-3 my-2">
+                <div className="card" style={{ width: "18rem" }}>
+                  <img src={newget.urlToImage} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">{newget.title}</h5>
+                    <p className="card-text">Description: {newget.description}</p>
                   </div>
                 </div>
               </div>
-            )
-          })
-        }
+            </div>
+          )
+        })}
       </div>
     </>
   );
